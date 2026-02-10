75488e46810e2100140aadca837f71ca6dbb3da2
dev-praveen <spraveen0244@gmail.com>
2026-02-10 19:57:16 +0530

    initial commit

---
+++ src/main/java/com/praveen/ai/AiPortfolioAnalyserApplication.java
@@ -0,0 +1,12 @@
+package com.praveen.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPortfolioAnalyserApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(AiPortfolioAnalyserApplication.class, args);
+  }
+}
